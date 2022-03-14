1c26943d879f3053d0ca8b8e2b25786fad5e7471
slash585 <slsh585@gmail.com>
2022-03-15 00:54:45 +0300

    Initializing app

---
+++ TinderClone/TinderClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderClone
+//
+//  Created by Mehmet Ali Özdemir on 14.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
+
+
